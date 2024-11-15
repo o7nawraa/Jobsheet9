121d199390ae999f1dc35243e5b53bb3def85d6d
07nawraa <anindyanauraanindya@gmail.com>
2024-11-15 11:01:12 +0700

    Modify program based on question

--- Jobsheet9/ArrayNumbers07.java
+++ Jobsheet9/ArrayNumbers07.java
@@ -5,9 +5,9 @@ public class ArrayNumbers07 {
         num[1] = 12;
         num[2] = 7;
         num[3] = 20;
-        System.out.println(num[0]);
-        System.out.println(num[1]);
-        System.out.println(num[2]);
-        System.out.println(num[3]);
+
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(num[i]);
+        }
     }
 }
