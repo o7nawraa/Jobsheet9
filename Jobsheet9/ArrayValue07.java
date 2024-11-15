898104634e2698625b91817aff637835b20744c7
07nawraa <anindyanauraanindya@gmail.com>
2024-11-15 12:45:16 +0700

    modify program so can display who pass and who fail

--- Jobsheet9/ArrayValue07.java
+++ Jobsheet9/ArrayValue07.java
@@ -4,12 +4,16 @@ public class ArrayValue07 {
         Scanner sc = new Scanner (System.in);
         int[] finalScore = new int[10];
 
-        for(int i = 0; i < 10; i++) {
+        for(int i = 0; i < finalScore.length; i++) {
             System.out.print("Enter the final score " + i + ": ");
             finalScore[i] = sc.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Final score " + i + " is " + finalScore[i]);
+        for (int i = 0; i < finalScore.length; i++) {
+           if(finalScore[i] > 70){
+               System.out.println("Student " + i + " Passed!");
+           } else {
+                System.out.println("Student " + i + " Failed!" );
+           }
         }
     }
 }
