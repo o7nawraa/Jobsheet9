fc53346ff9f1b6375ea9d85b9a2dd6806e1ceea6
07nawraa <anindyanauraanindya@gmail.com>
2024-11-15 21:54:03 +0700

    The answer of Modify code no 2 experiment 3

--- Jobsheet9/ArrayAverageScore07.java
+++ Jobsheet9/ArrayAverageScore07.java
@@ -2,24 +2,31 @@ import java.util.Scanner;
 public class ArrayAverageScore07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        int[] score = new int [10];
-        double total = 0;
-        double average;
-        int counter = 0;
 
+        double average;
+        double fail = 0, counterFail = 0, pass = 0, counterPass = 0;
+        
+        System.out.print("Enter the number of students: ");
+        int n = sc.nextInt();
+        
+        int[] score = new int [n];
         for (int i = 0; i < score.length; i++) {
-            System.out.print("Enter student score " + (i + 1) + ": ");
+            System.out.print("Enter the final score " + i + ": ");
             score[i] = sc.nextInt();
         }
         for (int i = 0; i < score.length; i++) {
             if (score[i]>70) {
-                System.out.println("Student Score " + (i+1) + " Is Passed!");
-                counter++;
-            }
-            total += score[i];
+                pass+=score[i];
+                counterPass++;
+            } else if (score[i]<70) {
+                fail+=score[i];
+                counterFail++;
+            }   
         } 
-        System.out.println("Total student who passed is " + counter);
-        average = total / score.length;
-        System.out.println("The class average score is " + average);
+        
+        average = pass / counterPass;
+        System.out.println("The average score of students who passed is " + average);
+        average = fail / counterFail;
+        System.out.println("The average score of student who failed is " + average);
     }
 }
