7ab2ed02826d1dd5ac9ac570b5d94cedf14181cf
07nawraa <anindyanauraanindya@gmail.com>
2024-11-15 10:05:06 +0700

    all about array 1d

---
+++ Jobsheet9/ArrayAverageScore07.java
@@ -0,0 +1,5 @@
+public class ArrayAverageScore07 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Jobsheet9/ArrayValue07.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ArrayValue07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int[] finalScore = new int[10];
+
+        for(int i = 0; i < 10; i++) {
+            System.out.print("Enter the final score " + i + ": ");
+            finalScore[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Final score " + i + " is " + finalScore[i]);
+        }
+    }
+}
