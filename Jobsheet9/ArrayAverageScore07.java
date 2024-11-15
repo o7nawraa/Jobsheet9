cf463a7fd6c8fd3f8027d27747bc453b5e011ee6
07nawraa <anindyanauraanindya@gmail.com>
2024-11-15 21:16:46 +0700

    Modify proram code no 1 experiment 3

--- Jobsheet9/ArrayAverageScore07.java
+++ Jobsheet9/ArrayAverageScore07.java
@@ -5,14 +5,20 @@ public class ArrayAverageScore07 {
         int[] score = new int [10];
         double total = 0;
         double average;
+        int counter = 0;
 
         for (int i = 0; i < score.length; i++) {
             System.out.print("Enter student score " + (i + 1) + ": ");
             score[i] = sc.nextInt();
         }
         for (int i = 0; i < score.length; i++) {
+            if (score[i]>70) {
+                System.out.println("Student Score " + (i+1) + " Is Passed!");
+                counter++;
+            }
             total += score[i];
-        }
+        } 
+        System.out.println("Total student who passed is " + counter);
         average = total / score.length;
         System.out.println("The class average score is " + average);
     }
